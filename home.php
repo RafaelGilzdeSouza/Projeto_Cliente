6a3c345d44d28ec4c2941a27d6c01ec0e77b1320
RafaelGilzdeSouza <gilzrafa@gmail.com>
2021-10-21 17:08:55 -0300

    remocao da funcao de redimencionamento de imagem

--- home.php
+++ home.php
@@ -6,25 +6,6 @@ $sql_code = "SELECT * FROM tb_produtos WHERE promocao = 1";
 $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
 
 
-function redimencionarImagemJPG($imagem, $largura, $altura)
-{
-// Cria um identificador para nova imagem
-$imagem_original = imagecreatefromjpeg($imagem);
-// Salva o tamanho antigo da imagem
-list($largura_antiga, $altura_antiga) = getimagesize($imagem);
-// Cria uma nova imagem com o tamanho indicado
-// Esta imagem servirá de base para a imagem a ser reduzida
-$imagem_tmp = imagecreatetruecolor($largura, $altura);
-// Faz a interpolação da imagem base com a imagem original
-imagecopyresampled($imagem_tmp, $imagem_original, 0, 0, 0, 0, $largura, $altura, $largura_antiga, $altura_antiga);
-// Salva a nova imagem
-$resultado = imagejpeg($imagem_tmp, 'imagem_nova.jpg');
-// Libera memoria
-imagedestroy($imagem_original);
-imagedestroy($imagem_tmp);
-
-}
-
 ?>
 
 
