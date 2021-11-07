43c44095592de3c0ff653f30b8121075ad7ebd91
RafaelGilzdeSouza <gilzrafa@gmail.com>
2021-11-07 16:47:19 -0300

    layout cadastros

--- home.php
+++ home.php
@@ -9,6 +9,7 @@ $GLOBALS['ID'] = $_SESSION['id_priv'];
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
+        
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
@@ -20,6 +21,8 @@ $GLOBALS['ID'] = $_SESSION['id_priv'];
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
 
+        <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+        
         <!-- Nucleo do CSS e JS -->
         <link href="css/styles.css" rel="stylesheet" />
         <script src="js/scripts.js"></script>
